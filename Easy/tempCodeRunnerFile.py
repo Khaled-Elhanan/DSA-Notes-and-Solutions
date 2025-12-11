dc2dce72b9eb491ee53b2a7dcbd4998b02dde8db
Khaled Elhanan <deathnotocode1@gmail.com>
2025-12-11 10:06:06 +0200

    remove temp file

--- Easy/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-        # c=0
-        # if i=="C":
-        #     res.pop()
-        # elif i=="+":
-        #     x+=[c+i for i in res]
-        #     res.append(x)
-        # elif i=="D":
-        #     x=[i*2 for i in res]
-        #     res.append(x)
